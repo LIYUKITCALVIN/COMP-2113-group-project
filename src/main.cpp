434d4c43582e4df144de8bcdb9b8d0f37c53189b
LIYUKITCALVIN <liyukit@connect.hku.hk>
2025-11-10 21:32:25 +0800

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Game.h"
+
+int main() {
+    std::cout << "Nuclear Ashes: Space Trader - Starting Game..." << std::endl;
+    Game game;
+    game.run();
+    return 0;
+}
