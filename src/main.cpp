afcd48042b2024b16ddd6f0d2b53e56657ca5d41
LIYUKITCALVIN <liyukit@connect.hku.hk>
2025-11-10 21:32:45 +0800

    Delete src/main.cpp

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Game.h"
-
-int main() {
-    std::cout << "Nuclear Ashes: Space Trader - Starting Game..." << std::endl;
-    Game game;
-    game.run();
-    return 0;
-}
